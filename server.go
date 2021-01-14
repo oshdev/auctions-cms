adc8a4e64daa60e995feece4f2edd6afeac0ed24
David Wickes <dave.wickes@gmail.com>
2021-01-14 11:44:21 +0000

    broken out into multiple routes

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 type Repo interface {
@@ -15,6 +16,7 @@ type Repo interface {
 type Server struct {
 	todoTemplate *template.Template
 	repo         Repo
+	router 		 *mux.Router
 }
 
 func NewServer(templateFolderPath string, repo Repo) (*Server, error) {
@@ -26,28 +28,39 @@ func NewServer(templateFolderPath string, repo Repo) (*Server, error) {
 			err,
 		)
 	}
-	return &Server{todoTemplate: todoTemplate, repo: repo}, nil
-}
+	router := mux.NewRouter()
 
-func (t *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		t.todoTemplate.Execute(w, t.repo.GetTodos())
-		return
-	}
+	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		todoTemplate.ExecuteTemplate(writer, "todo.gohtml", repo.GetTodos())
+	}).Methods(http.MethodGet)
 
-	if r.Method == http.MethodPost {
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "couldn't parse the form %v", err)
+	router.HandleFunc("/add", func(writer http.ResponseWriter, request *http.Request) {
+		todoTemplate.ExecuteTemplate(writer, "add.gohtml", repo.GetTodos())
+	}).Methods(http.MethodGet)
+
+	router.HandleFunc("/add", func(writer http.ResponseWriter, request *http.Request) {
+		if err := request.ParseForm(); err != nil {
+			fmt.Fprintf(writer, "couldn't parse the form %v", err)
 			return
 		}
+		repo.AddTodo(request.FormValue("new-item"))
 
-		if r.URL.Path == "/delete" {
-			id := r.FormValue("id")
-			t.repo.DeleteTodo(id)
-		} else {
-			t.repo.AddTodo(r.FormValue("new-item"))
+		http.Redirect(writer, request, "/", http.StatusSeeOther)
+	}).Methods(http.MethodPost)
+
+	router.HandleFunc("/delete", func(writer http.ResponseWriter, request *http.Request) {
+		if err := request.ParseForm(); err != nil {
+			fmt.Fprintf(writer, "couldn't parse the form %v", err)
+			return
 		}
+		id := request.FormValue("id")
+		repo.DeleteTodo(id)
+		http.Redirect(writer, request, "/", http.StatusSeeOther)
+	}).Methods(http.MethodPost)
 
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	}
+	return &Server{todoTemplate: todoTemplate, repo: repo, router: router}, nil
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.router.ServeHTTP(w, r)
 }
