131c357499de13858742962f8fafeefaa66b77ac
Chris James <qui666@gmail.com>
2021-01-17 09:27:30 +0000

    more furniture arranging

--- server.go
+++ server.go
@@ -25,13 +25,13 @@ func NewServer(templateFolderPath string, repo Repo) (*mux.Router, error) {
 			err,
 		)
 	}
-	router := mux.NewRouter()
 
 	server := server{
 		todoTemplate: todoTemplate,
 		repo:         repo,
 	}
 
+	router := mux.NewRouter()
 	router.HandleFunc("/", server.viewTodos).Methods(http.MethodGet)
 	router.HandleFunc("/add", server.viewAddTodoForm).Methods(http.MethodGet)
 	router.HandleFunc("/add", server.addTodo).Methods(http.MethodPost)
@@ -47,49 +47,53 @@ type server struct {
 	repo         Repo
 }
 
-func (s *server) viewTodos(writer http.ResponseWriter, r *http.Request) {
-	s.todoTemplate.ExecuteTemplate(writer, "todo.gohtml", s.repo.GetTodos())
+func (s *server) viewTodos(w http.ResponseWriter, r *http.Request) {
+	s.todoTemplate.ExecuteTemplate(w, "todo.gohtml", s.repo.GetTodos())
 }
 
-func (s *server) viewAddTodoForm(writer http.ResponseWriter, r *http.Request) {
-	s.todoTemplate.ExecuteTemplate(writer, "add.gohtml", s.repo.GetTodos())
+func (s *server) viewAddTodoForm(w http.ResponseWriter, r *http.Request) {
+	s.todoTemplate.ExecuteTemplate(w, "add.gohtml", s.repo.GetTodos())
 }
 
-func (s *server) addTodo(writer http.ResponseWriter, r *http.Request) {
+func (s *server) addTodo(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(1024)
 	s.repo.AddTodo(r.PostForm.Get("new-item"))
-	http.Redirect(writer, r, "/", http.StatusSeeOther)
+	redirectToHome(w, r)
 }
 
-func (s *server) deleteTodoStreamed(writer http.ResponseWriter, r *http.Request) {
+func (s *server) deleteTodoStreamed(w http.ResponseWriter, r *http.Request) {
 	id := getIdFromForm(r)
 
 	todo := s.repo.GetTodo(id)
 	s.repo.DeleteTodo(id)
-	writer.Header().Add("Content-Type", "text/vnd.turbo-stream.html")
+	w.Header().Add("Content-Type", "text/vnd.turbo-stream.html")
 
-	s.todoTemplate.ExecuteTemplate(writer, "toaster.partial.gohtml", todo)
-	s.todoTemplate.ExecuteTemplate(writer, "replace-todo-list-stream.gohtml", s.repo.GetTodos())
+	s.todoTemplate.ExecuteTemplate(w, "toaster.partial.gohtml", todo)
+	s.todoTemplate.ExecuteTemplate(w, "replace-todo-list-stream.gohtml", s.repo.GetTodos())
 }
 
-func (s *server) deleteTodo(writer http.ResponseWriter, r *http.Request) {
+func (s *server) deleteTodo(w http.ResponseWriter, r *http.Request) {
 	s.repo.DeleteTodo(getIdFromForm(r))
-	http.Redirect(writer, r, "/", http.StatusSeeOther)
+	redirectToHome(w, r)
 }
 
-func (s *server) viewEditTodoForm(writer http.ResponseWriter, r *http.Request) {
+func (s *server) viewEditTodoForm(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	s.todoTemplate.ExecuteTemplate(writer, "edit.gohtml", s.repo.GetTodo(id))
+	s.todoTemplate.ExecuteTemplate(w, "edit.gohtml", s.repo.GetTodo(id))
 }
 
-func (s *server) editTodo(writer http.ResponseWriter, r *http.Request) {
+func (s *server) editTodo(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	r.ParseMultipartForm(1024)
 
 	id := vars["id"]
 	s.repo.EditTodo(id, r.PostForm.Get("updated-name"))
-	http.Redirect(writer, r, "/", http.StatusSeeOther)
+	redirectToHome(w, r)
+}
+
+func redirectToHome(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func getIdFromForm(r *http.Request) string {
